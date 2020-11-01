061f12484388d0ac4ae99b6b6c0af4a338c2a4b7
진석진 <tjrwls9602@gmail.com>
2020-11-01 18:57:36 +0900

    20/11/01 TOPUI 애니메이터로 변경

--- WHY/Assets/Script/Hero.cs
+++ WHY/Assets/Script/Hero.cs
@@ -7,8 +7,7 @@ public class Hero : MonoBehaviour
 {
     void Update()
     {
-        transform.Rotate(0.0f, -50.0f * Time.deltaTime, 0.0f);  //회전 임시 -> 캐릭터로 바꿀때는 애니메이터 넣을거라 걍 눈 즐거움 용
-        transform.localScale = TOPUIManager.Instance.start.transform.localScale * 8;
+        transform.localScale = TOPUIManager.Instance.start.transform.localScale * 30;
 
         if (this.transform.localPosition.x < TOPUIManager.Instance.end.transform.localPosition.x)
         {
