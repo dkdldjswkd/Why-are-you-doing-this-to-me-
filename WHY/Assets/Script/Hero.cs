05e8dec150694c3b0d1c7eb4d0e70e4e874e03a4
진석진 <tjrwls9602@gmail.com>
2020-11-01 21:07:03 +0900

    20/11/01 TOPUI 강시처럼뛰는거 수정

--- WHY/Assets/Script/Hero.cs
+++ WHY/Assets/Script/Hero.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class Hero : MonoBehaviour
 {
+    public int size = 30;
+
     void Update()
     {
-        transform.localScale = TOPUIManager.Instance.start.transform.localScale * 30;
+        transform.localScale = TOPUIManager.Instance.start.transform.localScale * size;
 
         if (this.transform.localPosition.x < TOPUIManager.Instance.end.transform.localPosition.x)
         {
-            transform.localPosition = new Vector3(transform.localPosition.x + 30.0f * Time.deltaTime, TOPUIManager.Instance.start.transform.localPosition.y, 0);
+            transform.localPosition = new Vector3(transform.localPosition.x + 30.0f * Time.deltaTime, TOPUIManager.Instance.start.transform.localPosition.y-8, -20);
         }
         else
         {
