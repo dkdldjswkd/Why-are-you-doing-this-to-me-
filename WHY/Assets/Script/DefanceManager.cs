02947eed4bc3df04f10205c5f6eef7777ec7fead
dkdldjswkd <dkdldjswkd@naver.com>
2020-11-09 16:22:36 +0900

    asdf

--- WHY/Assets/Script/DefanceManager.cs
+++ WHY/Assets/Script/DefanceManager.cs
@@ -9,6 +9,8 @@ public class DefanceManager : MonoBehaviour
     public static float Cost; // 기물 구매, 래벨업에 상용되는 재화
     public static int Level; // 플레이어 레벨
 
+    public static bool SpawnerButtonDown; // 유닛스포너 버튼을 누른경우만 true;
+
     [SerializeField] GameObject Unit1;
     [SerializeField] GameObject Unit2;
     [SerializeField] GameObject Unit3;
@@ -27,6 +29,7 @@ public class DefanceManager : MonoBehaviour
         if (Production == 0) Production = 1;
         if (MaxCost == 0) MaxCost = 10;
         Level = 1;
+        SpawnerButtonDown = false;
     }
 
     // Update is called once per frame
@@ -56,37 +59,48 @@ public class DefanceManager : MonoBehaviour
 
     void SummonObject()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // 메인카메라에서 스크린으로 쏘는 레이저, 마우스 포지션 좌표에
-        RaycastHit hit;
-
-        //좌클릭시, ray가 물체에 충돌했으며, 충돌한 물체의 tag가 ground 라면
-        if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0) && hit.transform.tag == "Ground")
+        //유닛 스포너 버튼을 누른게 참일경우
+        if (SpawnerButtonDown)
         {
-            //돈이 1원 이상 있다면
-            if (Cost >= 1)
-            {
-                Cost--;
-                GameObject summondObject = null;
 
-                switch (Level)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // 메인카메라에서 스크린으로 쏘는 레이저, 마우스 포지션 좌표에
+            RaycastHit hit;
+
+            //좌클릭시, ray가 물체에 충돌했으며, 충돌한 물체의 tag가 ground 라면
+            if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0) && hit.transform.tag == "Ground")
+            {
+                //돈이 1원 이상 있다면
+                if (Cost >= 1)
                 {
-                    case 1:
-                        summondObject = Instantiate(Unit1, new Vector3(hit.point.x, 1, hit.point.z), Quaternion.identity);
-                        break;
-                    case 2:
-                        summondObject = Instantiate(Unit2, new Vector3(hit.point.x, 1, hit.point.z), Quaternion.identity);
-                        break;
-                    case 3:
-                        summondObject = Instantiate(Unit3, new Vector3(hit.point.x, 1, hit.point.z), Quaternion.identity);
-                        break;
-                }
-               
+                    Cost--;
+                    GameObject summondObject = null;
 
-            }
-            else
-            {
+                    switch (Level)
+                    {
+                        case 1:
+                            summondObject = Instantiate(Unit1, new Vector3(hit.point.x, 1, hit.point.z), Quaternion.identity);
+                            SpawnerButtonDown = false; // 유닛이 정상적으로 스폰됬기때문에 눌린 버튼을 다시 취소함
+                            break;
+                        case 2:
+                            summondObject = Instantiate(Unit2, new Vector3(hit.point.x, 1, hit.point.z), Quaternion.identity);
+                            SpawnerButtonDown = false; // 유닛이 정상적으로 스폰됬기때문에 눌린 버튼을 다시 취소함
+                            break;
+                        case 3:
+                            summondObject = Instantiate(Unit3, new Vector3(hit.point.x, 1, hit.point.z), Quaternion.identity);
+                            SpawnerButtonDown = false; // 유닛이 정상적으로 스폰됬기때문에 눌린 버튼을 다시 취소함
+                            break;
+                    }
+
+
+                }
+                else
+                {
+                }
             }
         }
+        
 
     }
+
+
 }
---
+++ WHY/Assets/Script/SpawnerButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerButton : MonoBehaviour
+{
+    bool* ClickedButton = new bool[4];
+
+    void Spawner1()
+    {
+        DefanceManager.SpawnerButtonDown = !DefanceManager.SpawnerButtonDown;
+    }
+}
