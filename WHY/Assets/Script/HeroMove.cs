18d9cb44fa68d5e3619c29343026fa146bd0dd8a
진석진 <tjrwls9602@gmail.com>
2020-12-02 01:44:11 +0900

    용사2개 파츠 완료 AI구현

--- WHY/Assets/Script/HeroMove.cs
+++ WHY/Assets/Script/HeroMove.cs
@@ -15,54 +15,73 @@ public class HeroMove : MonoBehaviour
     private float Powerspeed = 15f;
 
     [SerializeField]
-    Transform target;
+    Transform MainTarget;
+    [SerializeField]
+    Transform subtarget;
 
     public LayerMask targetmask;
-
+    
     void Start()
     {
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        subtarget = MainTarget;
     }
 
     void Update()
     {
         isRun();
-        agent.SetDestination(target.position);
+        agent.SetDestination(subtarget.position);
     }
 
     void isRun()
     {
-        Collider[] movecoll = Physics.OverlapSphere(transform.position, 50.0f, targetmask);
-
-        if (movecoll.Length != 0)
+        Collider[] attackcoll = Physics.OverlapSphere(transform.position, 15.0f, targetmask);
+        if (attackcoll.Length != 0)
         {
-            Collider[] attackcoll = Physics.OverlapSphere(transform.position, 15.0f, targetmask);
-            if (attackcoll.Length != 0)
+            print("어택 배열 수 :" + attackcoll.Length);
+            for (int i = 0; i < attackcoll.Length; i++)
             {
-                anim.SetBool("isRun", false);
-                anim.SetBool("isWalka", false);
-
-                anim.SetBool("isAttack", true);
-                agent.speed = 0.0f;
+                print(i + "번째 어택 콜라이더 :" + attackcoll[i].ToString());
             }
-            else
+
+            subtarget = attackcoll[attackcoll.Length - 1].transform;
+
+            anim.SetBool("isRun", false);
+            anim.SetBool("isWalka", false);
+
+            anim.SetBool("isAttack", true);
+            agent.speed = 0.0f;
+        }
+        else
+        {
+            Collider[] movecoll = Physics.OverlapSphere(transform.position, 50.0f, targetmask);
+            if (movecoll.Length != 0)
             {
+                print("런 배열 수 :" + movecoll.Length);
+                for (int i = 0; i < movecoll.Length; i++)
+                {
+                    print(i + "번째 런 콜라이더 :" + movecoll[i].ToString());
+                }
+                subtarget = movecoll[movecoll.Length - 1].transform;
                 anim.SetBool("isWalka", false);
                 anim.SetBool("isAttack", false);
 
                 anim.SetBool("isRun", true);
                 agent.speed = Powerspeed;
             }
-        }
-        else
-        {
-            anim.SetBool("isRun", false);
-            anim.SetBool("isAttack", false);
+            else
+            {
+                anim.SetBool("isRun", false);
+                anim.SetBool("isAttack", false);
 
-            anim.SetBool("isWalka", true);
-            agent.speed = speed;
+                anim.SetBool("isWalka", true);
+                agent.speed = speed;
+
+                subtarget = MainTarget;
+            }
         }
     }
-
 }
+
+
