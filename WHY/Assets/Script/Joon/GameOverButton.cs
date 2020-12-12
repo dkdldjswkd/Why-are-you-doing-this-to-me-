8e480bf9cda728f5b1052072f057d525c1803f6e
dkdldjswkd <dkdldjswkd@naver.com>
2020-12-13 05:29:06 +0900

    게임 종료 캔버스

---
+++ WHY/Assets/Script/Joon/GameOverButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameOverButton : MonoBehaviour
+{
+    public void GoMainScreen()
+    {
+        Time.timeScale = 1.0f; // 시간 멈춤 원상복구
+        SceneManager.LoadScene("MainMenu");
+        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
