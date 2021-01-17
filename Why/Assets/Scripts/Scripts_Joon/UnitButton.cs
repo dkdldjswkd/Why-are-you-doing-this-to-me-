207ffd1f4154bd65f5d86e146a2c83862cf8545a
dkdldjswkd <dkdldjswkd@naver.com>
2021-01-18 02:58:15 +0900

    미니언 배치 구현

---
+++ Why/Assets/Scripts/Scripts_Joon/UnitButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitButton : MonoBehaviour
+{
+    string Myname;
+    int MyNumber;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Myname = gameObject.name;
+        MyNumber = Myname[Myname.Length - 1] - '0';
+    }
+
+    public void SetReadyMinion()
+    {
+        DefanceManager.ReadyMinion = DefanceManager.HandMinions[MyNumber];
+        DefanceManager.ActivatedButtonNumber = MyNumber;
+    }
+
+}
