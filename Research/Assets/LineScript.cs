ba2602a6d661de720f0a3d37f9fce24bdcdc5ad2
진석진 <tjrwls9602@gmail.com>
2021-01-24 19:38:20 +0900

    마법그리기

---
+++ Research/Assets/LineScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour
+{
+    public GameObject linePerfab;
+    public GameObject linePoint;
+
+    LineRenderer lr;
+    EdgeCollider2D col;
+    List<Vector3> points = new List<Vector3>();
+    
+    void Start()
+    {
+        GameObject go = Instantiate(linePerfab);
+        lr = go.GetComponent<LineRenderer>();
+        col = go.GetComponent<EdgeCollider2D>();
+        points.Add(linePoint.transform.position);
+        lr.positionCount = 1;
+        lr.SetPosition(0, points[0]);
+    }
+
+
+    void Update()
+    {
+        Vector3 pos = linePoint.transform.position;
+        if (Vector3.Distance(points[points.Count - 1], pos) > 1.0f)
+        {
+            points.Add(pos);
+            lr.positionCount++;
+            lr.SetPosition(lr.positionCount - 1, pos);
+        }
+    }
+}
